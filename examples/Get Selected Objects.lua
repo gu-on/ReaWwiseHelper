75f24e6e30cecea5d985681560aef90c27744b14
gu-on <kevin@gu-on.com>
2024-12-18 19:45:45 +0000

    Add script to match the blog example

---
+++ examples/Get Selected Objects.lua
@@ -0,0 +1,18 @@
+package.path = package.path .. ';' .. debug.getinfo(1).source:match("@?(.*[\\|/])") .. '../?.lua'
+
+require('reawwisehelper')
+
+local client <close> = WaapiClient()
+if client then
+    local returns = "path"
+    local parameters = JsonMap("return", JsonArray(returns))
+
+    local isValid, result = client:Call("ak.wwise.ui.getSelectedObjects", JsonMap(), parameters)
+
+    if isValid then
+        local objects = result:GetJsonMapTable("objects")
+        for _, object in pairs(objects) do
+            reaper.ShowConsoleMsg(object:GetString(returns))
+        end
+    end
+end
