fb5310e1742e968c1b160270f51a5ab0def09d56
gu-on <kevin@gu-on.com>
2025-05-18 14:41:12 +0100

    Fixed website grabber script

--- scripts/waapi_getcommands.lua
+++ scripts/waapi_getcommands.lua
@@ -3,13 +3,13 @@
 package.path = package.path .. ';' .. debug.getinfo(1).source:match("@?(.*[\\|/])") .. '?.lua'
 local argparse = require "argparse"
 
-local parser = argparse("WAAPI Python to Lua", "Converts waapi_uri.py provided in Wwise SDK to a file that can be used by Lua Language Server")
+local parser = argparse("WAAPI Wwise Authoring Command Identifiers URL to Lua", "Grabs data from Wwise website and converts it to a file that can be used by Lua Language Server")
+parser:argument("input", "Wwise Authoring Command Identifiers (e.g. for 2024.1.5 = https://www.audiokinetic.com/en/public-library/2024.1.5_8803/?source=SDK&id=globalcommandsids.html)")
 parser:option("-o --output", "Output file.", "waapi_globalcommands.lua")
 -- add force command to get from web, even if file exists
 local args = parser:parse()
 
-local globalCommandsUrl = "https://www.audiokinetic.com/en/library/edge/?source=SDK&id=globalcommandsids.html"
-local pathIn = "globalcommands.tmp"
+local tempFile = "globalcommands.tmp"
 local pathOut = args.output
 
 ---@class command_info
@@ -18,33 +18,20 @@ local pathOut = args.output
 ---@field arg string # extra info
 
 do
-    if not io.open(pathIn, "r") then
-        local suc, _, code = os.execute("curl -i -o " .. pathIn .. " \"" .. globalCommandsUrl .. "\"")
+    if not io.open(tempFile, "r") then
+        local suc, _, code = os.execute(string.format("curl \"%s\" -o %s", args.input, tempFile))
         if not suc then
             error(("curl command was not successful. Returned %s"):format(code))
         end
     end
 
-    local fileIn <close> = io.open(pathIn, "r")
+    local fileIn <close> = io.open(tempFile, "r")
 
     if not fileIn then
-        error(("file could not be opened %s"):format(pathIn))
+        error(("file could not be opened %s"):format(tempFile))
     end
 
     local function contains(s, t) for _, v in pairs(t) do if s:find(v) then return true end end return false end
-    local function count(str, sub)
-        local num = 0
-        local pos = 1
-        local t = {}
-        while true do
-            local curPos = string.find(str, sub, pos)
-            if not curPos then break end
-            num = num + 1
-            pos = curPos + 1
-            table.insert(t, pos)
-        end
-        return num, t
-    end
 
     local rawlines = {} ---@type string[]
     do -- get raw lines from temp file
@@ -100,7 +87,7 @@ do
                 end
             end
 
-            do -- Get arg (if exists)
+            do -- todo: Get arg (if exists)
                 
             end
         end
@@ -113,6 +100,8 @@ do
         error("Could not open file for writing: " .. pathOut)
     end
 
+    file_out:write("-- @noindex\n")
+    file_out:write("\n")
     file_out:write("---@alias WwiseAuthoringCommandIdentifiers\n")
     for i, info in ipairs(output) do
         local line = string.format("---| \"%s\" %s", info.func, info.desc)
